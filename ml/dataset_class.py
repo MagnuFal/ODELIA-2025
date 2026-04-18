d1c58ab4f70ede870d6a0a2fea808ef9ad5bb5c9
Magnus S. Fallmyr <magnufal@stud.ntnu.no>
2026-04-18 12:15:10 +0200

    back to old dataset class

--- ml/dataset_class.py
+++ ml/dataset_class.py
@@ -7,15 +7,10 @@ import torch
 from monai.transforms import Compose, RandFlipd, RandRotate90d, RandGaussianNoised, Resized
 
 class ODELIA_DATASET(Dataset):
-    def __init__(self, annotation_file, img_dir):
+    def __init__(self, annotation_file, img_dir, transform = None):
         self.image_labels = pd.read_csv(annotation_file)
         self.img_dir = img_dir
-        self.transforms = Compose([
-            RandFlipd(keys = ["image"], prob=0.5),
-            RandRotate90d(keys = ["image"], prob=0.5),
-            RandGaussianNoised(keys = ["image"], prob = 1),
-            #Resized(keys=["image"], spatial_size=(64, 128, 128)),
-        ])
+        self.transforms = transform
 
     def __len__(self):
         return len(self.image_labels)
@@ -23,18 +18,15 @@ class ODELIA_DATASET(Dataset):
     def __getitem__(self, index):
         img_path = os.path.join(self.img_dir, self.image_labels.iloc[index, 0] + ".npy")
         arr = np.load(img_path)
-
-        if self.transforms:
-            volume = arr
-            data = {"image" : volume}
-            transform_data = self.transforms(data)
-            arr = np.asarray(transform_data["image"])
-
-        arr = arr[np.newaxis, ...]
-
+        difference_in_scans = 8 - arr.shape[0]
+        if difference_in_scans != 0:
+            padding = np.zeros((difference_in_scans, 32, 256, 256))
+            arr = np.concatenate([arr, padding], axis=0)
         image = torch.from_numpy(arr).float()
         label = self.image_labels.iloc[index, -1]
         label = torch.tensor(label, dtype=torch.long)
+        if self.transforms:
+            image = self.transforms(image)
         return image, label, self.image_labels.iloc[index, 0]
     
 class ODELIA_SKORCH_DATASET(Dataset):
